45aafe4d5930d02e874610e3841b07ca90f94434
Denis Potapov <fantom.r45@mail.ru>
2021-01-15 10:44:54 +0500

    Explicit Broadcasts

--- app/src/main/java/com/example/broadcastreceiverexample/ExampleBroadcastReceiver.kt
+++ app/src/main/java/com/example/broadcastreceiverexample/ExampleBroadcastReceiver.kt
@@ -8,11 +8,6 @@ import android.widget.Toast
 class ExampleBroadcastReceiver : BroadcastReceiver() {
 
     override fun onReceive(context: Context?, intent: Intent?) {
-
-        if ("com.example.broadcastreceiverexample.EXAMPLE_ACTION" == intent?.action) {
-            val receiverText =
-                intent.getStringExtra("com.example.broadcastreceiverexample.EXAMPLE_ACTION")
-            Toast.makeText(context, receiverText, Toast.LENGTH_SHORT).show()
-        }
+        Toast.makeText(context, "ExampleBroadcastReceiver triggered", Toast.LENGTH_SHORT).show()
     }
 }
--- app/src/main/java/com/example/broadcastreceiverexample/MainActivity.kt
+++ app/src/main/java/com/example/broadcastreceiverexample/MainActivity.kt
@@ -1,23 +1,11 @@
 package com.example.broadcastreceiverexample
 
-import android.content.IntentFilter
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
-
-    private val exampleBroadcastReceiver = ExampleBroadcastReceiver()
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        val filter = IntentFilter("com.example.broadcastreceiverexample.EXAMPLE_ACTION")
-        registerReceiver(exampleBroadcastReceiver, filter)
-    }
-
-    override fun onDestroy() {
-        super.onDestroy()
-        unregisterReceiver(exampleBroadcastReceiver)
     }
 }
