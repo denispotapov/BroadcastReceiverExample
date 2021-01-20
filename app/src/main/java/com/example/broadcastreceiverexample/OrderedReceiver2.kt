352d055bbd7ffe7d1ecdf8eb1bd2e08ff4e8922e
Denis Potapov <fantom.r45@mail.ru>
2021-01-20 11:43:56 +0500

    GoAsync + PendingResult

--- app/src/main/java/com/example/broadcastreceiverexample/OrderedReceiver2.kt
+++ app/src/main/java/com/example/broadcastreceiverexample/OrderedReceiver2.kt
@@ -4,26 +4,46 @@ import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
 import android.widget.Toast
+import android.os.Handler
+import android.os.SystemClock
 
 class OrderedReceiver2 : BroadcastReceiver() {
 
     override fun onReceive(context: Context?, intent: Intent?) {
-        var resultCode = resultCode
-        var resultData = resultData
-        val resultExtras = getResultExtras(true)
-        var stringExtra = resultExtras.getString("stringExtra")
+        val handler = Handler()
 
-        resultCode++
-        stringExtra += "-> OrderedReceiver2"
+        val pendingResult = goAsync()
 
-        val toastText =
-            "\"OrderedReceiver2 \n resultCode: $resultCode \n resultData: $resultData \n stringExtra: $stringExtra"
+        Thread(object : Runnable {
+            override fun run() {
 
-        Toast.makeText(context, toastText, Toast.LENGTH_LONG).show()
+                SystemClock.sleep(10 * 1000)
 
-        resultData = "OrderedReceiver2"
-        resultExtras.putString("stringExtra", stringExtra)
+                var resultCode = pendingResult.resultCode
+                var resultData = pendingResult.resultData
+                val resultExtras = pendingResult.getResultExtras(true)
+                var stringExtra = resultExtras.getString("stringExtra")
 
-        setResult(resultCode, resultData, resultExtras)
+                resultCode++
+                stringExtra += "-> OrderedReceiver2"
+
+                val toastText =
+                    "\"OrderedReceiver2 \n resultCode: $resultCode \n resultData: $resultData \n stringExtra: $stringExtra"
+
+                handler.post(object : Runnable {
+                    override fun run() {
+                        Toast.makeText(context, toastText, Toast.LENGTH_LONG).show()
+                    }
+                })
+
+                resultData = "OrderedReceiver2"
+                resultExtras.putString("stringExtra", stringExtra)
+
+                pendingResult.setResult(resultCode, resultData, resultExtras)
+                pendingResult.finish()
+            }
+        }).start()
+
+
     }
 }
